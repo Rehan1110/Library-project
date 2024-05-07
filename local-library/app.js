d468f210a4bbd61f5b280decdf23ad7eb5426682
Rehan Abbas <abbasrehan054@gmail.com>
2024-05-07 23:59:11 +0530

    Added the data to the database

--- local-library/app.js
+++ local-library/app.js
@@ -9,6 +9,15 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGO_URI;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
