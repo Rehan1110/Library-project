c47f27ba11177decaa6352e9669f2972e8f8ac7d
Rehan Abbas <abbasrehan054@gmail.com>
2024-05-08 23:30:53 +0530

    Completed the controllers

--- local-library/routes/index.js
+++ local-library/routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// GET home page.
+router.get("/", function (req, res) {
+  res.redirect("/catalog");
+});
+
+
 module.exports = router;
